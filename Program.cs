1ddf31ad04ae2305c7d9721840ac2344be919900
Hohm <Hohm@HOHM>
2024-01-22 21:29:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi();
+            SayHiUser("Nelca");
+            SayHiAge("Nelca",25);
+            Console.ReadLine();
+        }
+        static void SayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+        static void SayHiUser(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+        static void SayHiAge(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age + " years old");
+        }
+
+
+
+
+
+
+
+
+        
+    }
+}
